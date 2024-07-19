e3d8438ce22d05d11d0607bb5e4a4c122c046081
soapmactavish23 <ricknogueira1231@gmail.com>
2024-07-18 23:29:05 -0300

    16gLaJKcDHVHbccbo9ayPsopB6gA1pZB888m8q6g2T8x6kBm

--- lib/application/config/application_config.dart
+++ lib/application/config/application_config.dart
@@ -1,11 +1,26 @@
+import 'package:cuidapet_api/application/config/database_connection_configuration.dart';
 import 'package:dotenv/dotenv.dart';
+import 'package:get_it/get_it.dart';
 
 class ApplicationConfig {
   Future<void> loadConfigApplication() async {
-    var env = DotEnv(includePlatformEnvironment: true)..load();
-    final variavel = env['url_banco_de_dados'];
-    print(variavel);
+    _loadEnv();
+    _loadDatabaseConfig();
   }
 
-  // void _loadEnv() => DotEnv(includePlatformEnvironment: true)..load();
+  _loadEnv() => DotEnv(includePlatformEnvironment: true)..load();
+
+  void _loadDatabaseConfig() {
+    var env = _loadEnv();
+    final databaseConfig = DatabaseConnectionConfiguration(
+      host: env['DATABASE_HOST'] ?? env['databaseHost'],
+      user: env['DATABASE_USER'] ?? env['databaseUser'],
+      port: int.tryParse(env['DATABASE_PORT'] ?? '') ??
+          int.tryParse(env['databasePort']) ??
+          0,
+      password: env['DATABASE_PASSWORD'] ?? env['databasePassword'],
+      databaseName: env['DATABASE_NAME'] ?? env['databaseName'],
+    );
+    GetIt.I.registerSingleton(databaseConfig);
+  }
 }
---
+++ lib/application/database/database_connection.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:mysql1/src/single_connection.dart';
+
+import 'package:cuidapet_api/application/config/database_connection_configuration.dart';
+import 'package:cuidapet_api/application/database/i_database_connection.dart';
+
+class DatabaseConnection implements IDatabaseConnection {
+  final DatabaseConnectionConfiguration _configuration;
+  DatabaseConnection(this._configuration);
+
+  @override
+  Future<MySqlConnection> openConnection() async {
+    return MySqlConnection.connect(ConnectionSettings(
+      host: _configuration.host,
+      port: _configuration.port,
+      user: _configuration.user,
+      password: _configuration.password,
+      db: _configuration.databaseName,
+    ));
+  }
+}
