a88a15864255474b46c3bd26d77fa4e9caccbbda
soapmactavish23 <ricknogueira1231@gmail.com>
2024-07-29 20:50:23 -0300

    Configurando estrutura de Rotas (Shelf-Router)

--- lib/application/config/service_locator_config.config.dart
+++ lib/application/config/service_locator_config.config.dart
@@ -8,33 +8,33 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:get_it/get_it.dart' as _i1;
-import 'package:injectable/injectable.dart' as _i2;
+import 'package:get_it/get_it.dart' as _i174;
+import 'package:injectable/injectable.dart' as _i526;
 
-import '../database/database_connection.dart' as _i5;
-import '../database/i_database_connection.dart' as _i4;
-import 'database_connection_configuration.dart' as _i3;
+import '../database/database_connection.dart' as _i396;
+import '../database/i_database_connection.dart' as _i77;
+import 'database_connection_configuration.dart' as _i32;
 
 // initializes the registration of main-scope dependencies inside of GetIt
-_i1.GetIt $initGetIt(
-  _i1.GetIt getIt, {
+_i174.GetIt $initGetIt(
+  _i174.GetIt getIt, {
   String? environment,
-  _i2.EnvironmentFilter? environmentFilter,
+  _i526.EnvironmentFilter? environmentFilter,
 }) {
-  final gh = _i2.GetItHelper(
+  final gh = _i526.GetItHelper(
     getIt,
     environment,
     environmentFilter,
   );
-  gh.factory<_i3.DatabaseConnectionConfiguration>(
-      () => _i3.DatabaseConnectionConfiguration(
+  gh.factory<_i32.DatabaseConnectionConfiguration>(
+      () => _i32.DatabaseConnectionConfiguration(
             host: gh<String>(),
             user: gh<String>(),
             port: gh<int>(),
             password: gh<String>(),
             databaseName: gh<String>(),
           ));
-  gh.lazySingleton<_i4.IDatabaseConnection>(
-      () => _i5.DatabaseConnection(gh<_i3.DatabaseConnectionConfiguration>()));
+  gh.lazySingleton<_i77.IDatabaseConnection>(() =>
+      _i396.DatabaseConnection(gh<_i32.DatabaseConnectionConfiguration>()));
   return getIt;
 }
