57adb2f5a8a00c56998c248f806192109e4f9bff
soapmactavish23 <ricknogueira1231@gmail.com>
2024-07-29 21:05:33 -0300

    iniciando o conceito de middler

--- bin/server.dart
+++ bin/server.dart
@@ -1,24 +1,9 @@
 import 'dart:io';
 
-import 'package:cuidapet_api/modules/teste/teste_controller.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
 import 'package:cuidapet_api/application/config/application_config.dart';
-
-// Configure routes.
-final _router = Router()
-  ..get('/', _rootHandler)
-  ..get('/echo/<message>', _echoHandler);
-
-Response _rootHandler(Request req) {
-  return Response.ok('Hello, World!\n');
-}
-
-Response _echoHandler(Request request) {
-  final message = request.params['message'];
-  return Response.ok('$message\n');
-}
 
 void main(List<String> args) async {
   // Use any available host or container IP (usually `0.0.0.0`).
@@ -28,6 +13,17 @@ void main(List<String> args) async {
   final appConfig = ApplicationConfig();
   final router = Router();
   appConfig.loadConfigApplication(router);
+
+  var handler = const Pipeline()
+      .addMiddleware((innerHandler) {
+        print('Iniciando 1 middle');
+        return (Request request) async {
+          print('');
+          return innerHandler(request);
+        };
+      })
+      .addMiddleware(logRequests())
+      .addHandler(router.call);
 
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
   final server = await serve(router.call, ip, port);
